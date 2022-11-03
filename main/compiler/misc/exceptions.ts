9ec06f02381ae938a99f611673be7c465a034253
tshpigel <96367527+tshpigel@users.noreply.github.com>
2022-11-02 20:18:13 -0600

    Update exceptions.ts

--- main/compiler/misc/exceptions.ts
+++ main/compiler/misc/exceptions.ts
@@ -1,18 +1,44 @@
+import { lookAheadReg } from "./general/looks.ts";
+import { fname } from "../process/lexer.ts";
+
 export const ExcArgs = (exc: string, ...args: string[]) => { // replace all placeholders with arguments
-  let final = exc; // final output
-  let rc = 0; // replace count
-  while(final.includes("¬")) {
-    final = final.replace("¬", args[rc++]); // replace placeholder with proper argument and add to replace count
-  }
-  return final;
+    let final = exc; // final output
+    let rc = 0; // replace count
+    while(final.includes("¬")) {
+        final = final.replace("¬", args[rc++]); // replace placeholder with proper argument and add to replace count
+    }
+    return final;
 };
 
-enum Exceptions {
-  UnknownLiteral = 'Unknown literal ¬',
-  UnreachableIndex = 'Unreachable ¬ in ¬ at index ¬',
-  NonArrayIndexing = 'Invalid indexing with ¬ as its type is a ¬ instead of an array',
-  SingleQuotedString = 'Invalid character length within single quotes',
-  DoubleQuotedCharacter = 'Character variable with double quotes',
-  CannotNil = 'Literal ¬ cannot be Nil',
-  PathInexistent = 'Path ¬ does not exist'
+export enum Exceptions {
+    UnknownLiteral = "Unknown literal '¬'",
+    UnreachableIndex = "Unreachable '¬' in '¬' at index '¬'",
+    NonArrayIndexing = "Invalid indexing with '¬' as its type is a '¬' instead of an array",
+    SingleQuotedString = "Invalid character length within single quotes",
+    DoubleQuotedCharacter = "Character variable with double quotes",
+    CannotNil = "Literal '¬' cannot be Nil",
+    PathInexistent = "Path ¬ does not exist",
+    InexistentKtype = "Kinetype '¬' does not exist",
+    InexistentMod = "Modifier '¬' does not exist",
+    InvalidModCase = "Modifier must contain only uppercase",
+    DuplicateVarName = "Variable name '¬' is already in use",
+    IncompatibleType = "'¬' type is not compatible with '¬' type",
+    RequiredType = "Variable '¬' must be associated with a data type if not previously initialized"
+}
+
+function strRep(char: string, count: number): string {
+    return Array(count).fill(char).join('');
+}
+
+export function exception(excType: keyof typeof Exceptions, args: string[], l: number, c: number, file: string, step: 'Lex' | 'Pars' | 'Emitt' | 'Typ'): void {
+    if(!Object.keys(Exceptions).includes(excType)) throw Error("Unidentifiable Error " + excType);
+    console.log(`%c${excType + ` ${step}ing Exception`} %c[errno. ${Object.keys(Exceptions).findIndex(e => e === excType)}]: %c${ExcArgs((Exceptions as Record<string, string>)[excType], ...args)}`, "color:red;font-weight:bold;font-size:30px;", "color:darkblue;", "color:#ff7675;");
+    console.log(`   %cat [ %c${import.meta.url.slice(0, import.meta.url.lastIndexOf("/") + 1) + `%c${fname}`}%c:%c${l}%c:%c${c} %c]`, "font-weight:bold;", "color:magenta;", "font-weight:bold;color:magenta;", "", "color:yellow;", "", "color:yellow;", "font-weight:bold;");
+    const line: string = file.split('\n')[l - 1];
+    const excepted: string = lookAheadReg(line, /[^\s]/, c).join(''); 
+    const excLen: number = excepted.length;
+    const [beforelen, afterlen] = [line.slice(0, c), line.slice(c + excLen)].map(e => e.length);
+    console.log(`\n      ${line}`);
+    console.log("      " + strRep(" ", beforelen) + `%c${strRep("^", excLen)}` + strRep(" ", afterlen), "color:red;outline:black;");
+    Deno.exit(1);
 }
