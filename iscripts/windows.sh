2b76ccd4d7558498ec77666e162066e81f4a4372
tshpigel <96367527+tshpigel@users.noreply.github.com>
2023-01-22 16:19:30 -0700

    Update windows.sh

--- iscripts/windows.sh
+++ iscripts/windows.sh
@@ -2,9 +2,10 @@
 if deno --version; then
     irm https://deno.land/install.ps1 | iex
 fi
-if test -f ~/.bytl.bash; then
-touch ~/.bytl.bash
-cat > ~/.bytl.bash << "EOF"
+if test -f ~\.bytl.bash; then
+mv ~\Downloads\Bytl-main ~
+touch ~\.bytl.bash
+cat > ~\.bytl.bash << "EOF"
 #!/bin/bash
 function bytl() {
     a1="$1"
@@ -12,9 +13,9 @@ function bytl() {
         [[ -z "$2" && "$2" != "no" ]] && arg="$a1" || arg="$2"
         deno run --allow-read --allow-write "$1" "$arg"
     }
-    rdeno ~/Bytl-main/main/compiler/process/main.ts "$1"
+    rdeno ~\Bytl-main\main\compiler\process\main.ts "$1"
     if [ "$?" == 0 ]; then
-        rdeno "./out/$a1.ts" no
+        rdeno "./out\$a1.ts" no
     fi
 }
 EOF
