a53c769bffa2af6c1bdcb6e29669f1b9e641c034
tshpigel <96367527+tshpigel@users.noreply.github.com>
2023-01-23 09:37:00 -0700

    Update and rename windows.sh to windows.ps1

---
+++ iscripts/windows.ps1
@@ -0,0 +1,21 @@
+#!/bin/sh
+If (deno --version) {
+    irm https://deno.land/install.ps1 | iex
+}
+
+If ([System.IO.File]::Exists(~\.bytl.bash)) {
+    New-item ~\.bytl.bash -type file
+    "Function bytl(`$file) { 
+        Function Rdeno(`$tsfile `$opt) {
+            `$arg = If(`$opt -eq `$null -and `$opt -ne 'no') {`$file} Else {`$opt}
+            deno run --allow-read --allow-write `$tsfile `$arg
+        }
+        Rdeno ~\bytl\main\process\main.ts `$file
+        If(`$? -eq 0) {
+            Rdeno './out\`$file.ts' no
+        }
+    }"
+} Else {
+    echo "Bytl is already installed"
+}
+source ~\.bytl.bash
