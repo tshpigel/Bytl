a94a0fb5a34051c4820a9f4480f9cb1395ddc21a
tshpigel <96367527+tshpigel@users.noreply.github.com>
2023-01-24 10:38:41 -0700

    Update windows.ps1

--- iscripts/windows.ps1
+++ iscripts/windows.ps1
@@ -2,16 +2,16 @@ If (! where deno | out-null ) {
     irm https://deno.land/install.ps1 | iex
 }
 
-If ([System.IO.File]::Exists($env:USERPROFILE\.bytl.bash)) {
-    Move-item $env:USERPROFILE\Downloads\Bytl-main $env:USERRPROFILE
-    New-item $env:USERPROFILE\.bytl.bash -type file
+If ([System.IO.File]::Exists($env:PROGRAMFILES\.bytl.ps1)) {
+    Move-item $env:USERPROFILE\Downloads\Bytl-main $env:PROGRAMFILES
+    New-item $env:PROGRAMFILES\.bytl.ps1 -type file
 @"
     Function bytl(`$file) { 
         Function Rdeno(`$tsfile `$opt) {
             `$arg = If(`$opt -eq `$null -and `$opt -ne 'no') {`$file} Else {`$opt}
             deno run --allow-read --allow-write `$tsfile `$arg
         }
-        Rdeno `$env:USERPROFILE\Bytl-main\main\compiler\process\main.ts `$file
+        Rdeno `$env:PROGRAMFILES\Bytl-main\main\compiler\process\main.ts `$file
         If(`$? -eq 0) {
             Rdeno './out\`$file.ts' no
         }
@@ -21,4 +21,4 @@ If ([System.IO.File]::Exists($env:USERPROFILE\.bytl.bash)) {
 } Else {
     "Bytl is already setup"
 }
-. $env:USERPROFILE\.bytl.bash
+. $env:PROGRAMFILES\.bytl.bash
