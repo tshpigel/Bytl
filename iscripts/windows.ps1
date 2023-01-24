4165eb835af369518216a393e8bd2b10a579d1ee
tshpigel <96367527+tshpigel@users.noreply.github.com>
2023-01-23 18:37:42 -0700

    Update windows.ps1

--- iscripts/windows.ps1
+++ iscripts/windows.ps1
@@ -3,15 +3,15 @@ If (!(deno --version)) {
 }
 
 If ([System.IO.File]::Exists(C:\Users\.bytl.bash)) {
-    Move-item %homepath%%homedrive%\Downloads\Bytl-main %homepath%%homedrive%
-    New-item %homepath%%homedrive%\.bytl.bash -type file
+    Move-item $env:USERPROFILE\Downloads\Bytl-main %homepath%%homedrive%
+    New-item $env:USERPROFILE\.bytl.bash -type file
 @"
     Function bytl(`$file) { 
         Function Rdeno(`$tsfile `$opt) {
             `$arg = If(`$opt -eq `$null -and `$opt -ne 'no') {`$file} Else {`$opt}
             deno run --allow-read --allow-write `$tsfile `$arg
         }
-        Rdeno %homepath%%homedrive%\Bytl-main\main\compiler\process\main.ts `$file
+        Rdeno `$env:USERPROFILE\Bytl-main\main\compiler\process\main.ts `$file
         If(`$? -eq 0) {
             Rdeno './out\`$file.ts' no
         }
@@ -20,4 +20,4 @@ If ([System.IO.File]::Exists(C:\Users\.bytl.bash)) {
 } Else {
     echo "Bytl is already installed"
 }
-. %homepath%%homedrive%\.bytl.bash
+. $env:USERPROFILE\.bytl.bash
