8c145b5a70b2fab4fa297ecd607a45384c15b1e7
tshpigel <96367527+tshpigel@users.noreply.github.com>
2023-01-23 18:26:34 -0700

    Update windows.ps1

--- iscripts/windows.ps1
+++ iscripts/windows.ps1
@@ -2,19 +2,21 @@ If (!(deno --version)) {
     irm https://deno.land/install.ps1 | iex
 }
 
-If ([System.IO.File]::Exists(~\.bytl.bash)) {
-    New-item ~\.bytl.bash -type file
-    @"Function bytl(`$file) { 
+If ([System.IO.File]::Exists(C:\Users\.bytl.bash)) {
+    New-item C:\Users\.bytl.bash -type file
+@"
+    Function bytl(`$file) { 
         Function Rdeno(`$tsfile `$opt) {
             `$arg = If(`$opt -eq `$null -and `$opt -ne 'no') {`$file} Else {`$opt}
             deno run --allow-read --allow-write `$tsfile `$arg
         }
-        Rdeno ~\bytl\main\process\main.ts `$file
+        Rdeno C:\Users\bytl\main\process\main.ts `$file
         If(`$? -eq 0) {
             Rdeno './out\`$file.ts' no
         }
-    }"@
+    }
+"@
 } Else {
     echo "Bytl is already installed"
 }
-source ~\.bytl.bash
+source C:\Users\.bytl.bash
